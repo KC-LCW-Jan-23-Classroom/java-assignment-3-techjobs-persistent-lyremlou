1e2363c1f9aee5d465610e0ed3c2f9ebf98d53ca
marysanders <lyremary@yahoo.com>
2023-09-10 12:36:14 -0500

    Update location field in Employer.java with NotNull annotation

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -2,14 +2,16 @@ package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
 public class Employer extends AbstractEntity {
 
-    @NotEmpty(message="Field required")
+    @NotNull(message="Field required")
     @Size(max=500, message="Description too long")
     private String location;
+
 
     public Employer(String location) {
         this.location = location;
